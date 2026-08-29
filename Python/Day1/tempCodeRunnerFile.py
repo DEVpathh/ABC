ca058a640d669c62d7c5ae381b05caed1b5e5517
DEVpathh <mukulpathakop@gmail.com>
2026-08-29 11:05:42 +0530

    Delete Python directory

--- Python/Day1/tempCodeRunnerFile.py
+++
@@ -1,1 +0,0 @@
-print("The length of "+name+" is "+length)
--- Python/Day2/bmi.py
+++
@@ -1,4 +0,0 @@
-height = float(input("Enter your height\n"))
-weight = int(input("Enter your weigth\n"))
-bmi = float(weight/height**2)
-print("your bmi is ",bmi,"kg/m^2")
